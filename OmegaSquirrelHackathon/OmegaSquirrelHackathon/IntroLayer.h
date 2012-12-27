7a02df35f6cfad5ee32ff09f31e854d1e2967d1f
Colden Prime <colden@intrepid.io>
2012-12-27 16:05:46 -0500

    add project file for hackathon

---
+++ OmegaSquirrelHackathon/OmegaSquirrelHackathon/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  OmegaSquirrelHackathon
+//
+//  Created by Colden Prime on 12/27/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
